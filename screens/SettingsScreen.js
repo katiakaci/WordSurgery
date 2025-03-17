06a7f3c6be17e7695d212f6c8dbc72e56f7f1b13
katia-kaci <katia.kaci.1@ens.etsmtl.ca>
2025-03-17 14:57:58 -0400

    deux boutons dans les settings

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { View, Text, StatusBar, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, StatusBar, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
 
-function SettingsScreen() {
+function SettingsScreen({ navigation }) {
   return (
-    <>
-        <Text style={styles.text}>Pour jouer, il faut ...</Text>
-        <StatusBar style="auto" />
-    </>
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+
+      <TouchableOpacity style={[styles.button]} onPress={() => {
+        // TODO Code pour changer la langue ici
+      }}
+      >
+        <Text style={styles.buttonText}>Langue</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.button]} onPress={() => {
+        // TODO Code pour changer le dictionnaire ici
+      }}
+      >
+        <Text style={styles.buttonText}>Changer de dictionnaire</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
@@ -15,10 +28,30 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#fff',
   },
-  text: {
-    color: 'black',
-    fontSize: 20,
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 40,
+  },
+  button: {
+    margin: 10,
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 5,
+    width: 250,
+    height: 80,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 2,
+    borderColor: '#000',
+    backgroundColor: '#fdb441'
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 
