0c2a538e62d99a53f416caf187ad8808a38fbe5f
katia-kaci <katia.kaci.1@ens.etsmtl.ca>
2025-03-17 01:08:21 -0400

    tutoriel défilement image

--- screens/TutorielScreen.js
+++ screens/TutorielScreen.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { View, Text, StatusBar, StyleSheet } from 'react-native';
+import { View, Text, StatusBar, StyleSheet, FlatList, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 function TutorielScreen() {
+  const navigation = useNavigation();
+
+  const images = [
+    require('../assets/instruction 1.jpg'),
+    require('../assets/instruction 2.jpg'),
+    require('../assets/instruction 3.jpg'),
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Pour jouer, il faut ...</Text>
       <StatusBar style="auto" />
+
+      <FlatList
+        data={images}
+        renderItem={({ item }) => (
+          <Image source={item} style={styles.image} />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        horizontal
+        pagingEnabled
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('WordPress')}
+      >
+        <Text style={styles.buttonText}>Jouer !</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,9 +38,26 @@ function TutorielScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff', // Couleur de fond, ajustez comme vous le souhaitez
+  },
+  image: {
+    width: 300, // Taille de l'image, ajustez selon vos besoins
+    height: 400, // Taille de l'image
+    marginHorizontal: 10, // Espacement horizontal entre les images
+    resizeMode: 'contain', // Assurez-vous que l'image garde ses proportions
+  },
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#4CAF50', // Couleur du bouton
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
 
