4461372fb95fcb829969952e55a361b03206ad00
katia-kaci <katia.kaci.1@ens.etsmtl.ca>
2025-03-16 23:55:36 -0400

    Page de chargement avec logo

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { useState, useEffect } from 'react';
 import RandomWord from './RandomWord';
 
 export default function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowWelcome(false);
+    }, 3000);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <RandomWord />
-      <StatusBar style="auto" />
+      {showWelcome ? (
+        <View style={styles.welcomeContainer}>
+          <Image source={require('./assets/app_icon.png')} style={styles.image} />
+          <Text style={styles.title}>WordPress</Text>
+        </View>
+      ) : (
+        <>
+          <RandomWord />
+          <StatusBar style="auto" />
+        </>
+      )}
     </View>
   );
 }
@@ -18,5 +35,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  welcomeContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
